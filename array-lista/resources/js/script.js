d58925806f1ed808fcc35da8bbaed1128d9580d8
Valerio-Catellani <valeriocatellani@gmail.com>
2024-03-12 13:21:03 +0100

    prima-commit. created all folders

---
+++ array-lista/resources/js/script.js
@@ -0,0 +1,21 @@
+
+
+
+const userInput = document.getElementById("user-input");
+const sendButton = document.getElementById("send-button");
+const cancelButton = document.getElementById("cancel-button");
+const response = document.getElementById("response");
+
+
+
+
+
+sendButton.addEventListener('click', function () {
+
+})
+cancelButton.addEventListener('click', function () {
+    userInput.value = response.innerHTML = "";
+})
+
+
+
