09526f5ddad93ed1080ac61ba8c57ce72fbec022
Valerio-Catellani <valeriocatellani@gmail.com>
2024-03-12 15:17:40 +0100

    first part of array-tail

--- array-tail/resources/js/script.js
+++ array-tail/resources/js/script.js
@@ -1,21 +1,39 @@
 
+// 2. La coda dell'Array
+// Cartella: array_tail
+// Creiamo un array chiedendo all'utente quanti elementi dovrà contenere e generando tanti numeri casuali (compresi nell'intervallo da 1 a 100) per quanti sono gli elementi da inserire.
+// Stampiamo poi gli ultimi 5 elementi dell'Array
+// Bonus:
+// Chiediamo all'utente quanti elementi dell'array dovremo stampare
+// Suggerimenti:
+// Probabilmente conviene controllare che il numero di elementi della coda non sia più grande del numero di elementi totali dell'array
+
+//input
+let numberOfElements = document.getElementById("number-of-elements");
+//buttons
+let generateArrayButton = document.getElementById("generate-array-button");
+let cancelButton = document.getElementById("cancel-button");
+//output
+let response = document.getElementById("response");
+let arrayOutput = document.querySelector("h4")
 
 
 
-const userInput = document.getElementById("user-input");
-const sendButton = document.getElementById("send-button");
-const cancelButton = document.getElementById("cancel-button");
-const response = document.getElementById("response");
+generateArrayButton.addEventListener('click', function () {
+    let elements = parseInt(numberOfElements.value)
+    if (isNaN(elements)) {
+        alert("Devi inserire un numero valido!")
+    } else {
+        let arrayRandom = [];
+        for (let i = 0; i < elements; i++) {
+            arrayRandom.push(getRndInteger(1, 100))
+        }
+        arrayOutput.innerHTML = `[${arrayRandom}]`;
+    }
 
-
-
-
-
-sendButton.addEventListener('click', function () {
 
 })
-cancelButton.addEventListener('click', function () {
-    userInput.value = response.innerHTML = "";
+cancelButton2.addEventListener('click', function () {
+    userInput2.value = "";
+    response2.innerHTML = "";
 })
-
-
